5c6124701ebb3ed726dd591612de8ed9f5882626
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-11-02 17:05:49 +0300

    mistake

--- redux/odev2/gatesmoney/src/Components/Products.js
+++ redux/odev2/gatesmoney/src/Components/Products.js
@@ -6,44 +6,80 @@ import {useSelector} from 'react-redux'
 
 
 function Products() {
-    const allProducts = [{
+    const allProductsTemp = [{
         id: 1,
         name: "Big Mac",
         price: 2,
-        img: "https://neal.fun/spend/images/big-mac.jpg"
+        img: "https://neal.fun/spend/images/big-mac.jpg",
+        piece: 5
     },
-{
-    id: 2,
-    name: "Coca-Cola Pack",
-    price: 5,
-    img: "https://neal.fun/spend/images/coca-cola-pack.jpg"
-},
-{
-    id: 3,
-    name: "Book",
-    price: 15,
-    img: "https://neal.fun/spend/images/book.jpg"
-},
-{
-    id: 4,
-    name: "Video Game",
-    price: 60,
-    img: "https://neal.fun/spend/images/video-game.jpg"
-},
-{
-    id: 5,
-    name: "Year of Netflix",
-    price: 100,
-    img: "https://neal.fun/spend/images/year-of-netflix.jpg"
-},
-];
+    {
+        id: 2,
+        name: "Coca-Cola Pack",
+        price: 5,
+        img: "https://neal.fun/spend/images/coca-cola-pack.jpg",
+        piece: 0
+    },
+    {
+        id: 3,
+        name: "Book",
+        price: 15,
+        img: "https://neal.fun/spend/images/book.jpg",
+        piece: 0
+    },
+    {
+        id: 4,
+        name: "Video Game",
+        price: 60,
+        img: "https://neal.fun/spend/images/video-game.jpg",
+        piece: 0
+    },
+    {
+        id: 5,
+        name: "Year of Netflix",
+        price: 100,
+        img: "https://neal.fun/spend/images/year-of-netflix.jpg",
+        piece: 0
+    },
+    ];
 
+
+const [allProducts, setAllProducts] = useState(allProductsTemp)
 const moneyLeft = useSelector((state) => state.money.moneyleft)
 const [inputCount, setinputCount] = useState(0)
+// const basketInit = [{
+//     id: 0,
+//     count: 0,
+// }];
+const basketInit = [];
+const basket = basketInit
 
 const buyButtonHandle = (e) => {
     // console.log(`${inputCount} adet, ${prom})
-    console.log(e)
+    // console.log(inputCount)
+    // setinputCount(inputCount +1 )
+
+    // const itemBas = allProducts.find((item) => item.id == e.target.name)
+    // itemBas.piece = itemBas.piece + 1;
+    // basket.push(itemBas)
+    // console.log(basket)
+
+    // const itemBas = allProducts.find((item) => item.id == e.target.name)
+    // setAllProducts([...allProducts, allProducts.map((item) => {
+    //     if (item.id == e.target.name)
+    //     {
+    //     item.piece = item.piece + 1;
+        
+    //     }
+    // })])
+    
+    allProductsTemp[e.target.name -1].piece =  allProductsTemp[e.target.name -1 ].piece + 1;
+    setAllProducts([...allProductsTemp]);
+    
+    console.log(allProductsTemp);
+    console.log(allProducts)
+
+
 }
 
 const sellButtonHandle = () => {
@@ -51,7 +87,7 @@ const sellButtonHandle = () => {
 }
 
 const inputChangeHandle = (e) => {
-    setinputCount(e.target.value)
+    // setinputCount(e.target.value)
 
 }
 
@@ -60,14 +96,14 @@ const inputChangeHandle = (e) => {
         <h3 className='moneyLeft' >${moneyLeft}</h3>
         {allProducts.map((item) => {
             return (
-                <div className='test1'>
-                <div key={item.name} className='products'>
+                <div key={item.id+"test"} className='test1'>
+                <div key={item.name+"productsDiv"} className='products'>
                     <img className='productImage' key={item.img} src={item.img}></img>
-                    <h2 key={item.id}>{item.name}</h2>
+                    <h2 key={item.name}>{item.name}</h2>
                     <h2 key={item.price}>${item.price}</h2>
-                    <button key={item.id} className='sellBtn' onClick={sellButtonHandle}>Sell</button>
-                    <input onChange={inputChangeHandle}  className='itemCount' type="number" value={inputCount}></input>
-                    <button propt={item.id} key={item.id} className='buyBtn' onClick={buyButtonHandle}>Buy</button>
+                    <button key={item.id+"sellbtn"} className='sellBtn' onClick={sellButtonHandle}>Sell</button>
+                    <input onChange={inputChangeHandle} className='itemCount'  value={item.piece}></input>
+                    <button name={item.id} key={item.id+"buybtn"} className='buyBtn' onClick={buyButtonHandle}>Buy</button>
                 </div>
                 </div>
             )
