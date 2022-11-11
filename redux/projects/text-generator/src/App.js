0f5c45e3afd8c1a65e0157abedf544020cc6770b
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-11-11 21:36:06 +0300

    text-generator updated

--- redux/projects/text-generator/src/App.js
+++ redux/projects/text-generator/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>React sample text generator app</h1>
+    <hr></hr>
+
+      <div className="general">
+        <div className="paragraphs">
+          <h5>Paragraphs</h5>
+          <input value="4" type="number"></input>
+        </div>
+        <div className="inhtml">
+          <h5>include HTML</h5>
+          <input type="text"  ></input>
+        </div>
+        
+      </div>
+      <div className="text">
+          <label>asdasdasdasdas sadas asdas lorem</label>
+        </div>
+     
     </div>
   );
 }
