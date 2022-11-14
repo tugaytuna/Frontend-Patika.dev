0fb4c8546ec6a1ad039eab0b1248fec81067f4d3
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-11-14 19:41:00 +0300

    updated

--- redux/projects/markdownpreviewer/src/App.js
+++ redux/projects/markdownpreviewer/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react'
+
 function App() {
+
+
+const [markdownSnip, setMarkdownSnip] = useState(`Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a
+line break
+
+Text attributes *italic*, **bold**,
+'monospace', ~~strikethrough~~ .
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in
+Spain.
+
+ *[Herman Fassett](https://freecodecamp.com/hermanfassett)*`)
+
+
+const [inputText, setinputText] = useState("")
+
+const clickedHint = () => {
+  setinputText(markdownSnip)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='allB'>
+      <h1 className='headerT'>Markdown Previewer</h1>
+
+      <div className='textandmark'>
+        <input value={inputText} onChange={(e) => {setinputText(e.target.value)}}  className='text' type="text"></input>
+        <div className='markdown'>
+          markdown
+        </div>
+      </div>
+
+      <button onClick={clickedHint} className='question'>?</button>
     </div>
   );
 }
