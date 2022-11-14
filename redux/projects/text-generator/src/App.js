3ad5131d890ff30a80dd7938f033ac7637a4ad0e
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-11-14 17:27:17 +0300

    done project1

--- redux/projects/text-generator/src/App.js
+++ redux/projects/text-generator/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios, {isCancel, AxiosError} from 'axios';
 
 
 function App() {
 
+const [parag, setParag] = useState("");
+const [paragCount, setParagcount] = useState(4);
+const [paragAPI, setParagAPI] = useState("init value");
+const [selectedHTML, setselectedHTML] = useState(1);
 
+useEffect(() => {
+  const urlAPI = `https://baconipsum.com/api/?type=all-meat&paras=${paragCount}&format=${(selectedHTML) ? "html" : "text"}`
+
+
+  axios.get(urlAPI)
+  .then(function (response) {
+    // handle success
+    setParagAPI(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+
+},[paragCount, selectedHTML])
+
+
+const changeParagCount = (e) => {
+  setParagcount(e.target.value)
+
+}
 
 
   return (
@@ -14,16 +45,17 @@ function App() {
       <div className="general">
         <div className="paragraphs">
           <h5>Paragraphs</h5>
-          <input value="4" type="number"></input>
+          <input value={paragCount} onChange={changeParagCount}  type="number"></input>
         </div>
         <div className="inhtml">
           <h5>include HTML</h5>
-          <input type="text"  ></input>
+          <button className={(selectedHTML) ? "yesbutton selectedButton" : "yesbutton"} onClick={() => {setselectedHTML(1)}}>YES</button>
+          <button className={(!selectedHTML) ? "nobutton selectedButton" : "nobutton"} onClick={() => {setselectedHTML(0)}} >NO</button>
         </div>
         
       </div>
       <div className="text">
-          <label>asdasdasdasdas sadas asdas lorem</label>
+          <label>{paragAPI}</label>
         </div>
      
     </div>
@@ -31,3 +63,4 @@ function App() {
 }
 
 export default App;
+
