30d33fddd07517b9d073abfdf3d88134a3d19f43
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-12-13 00:52:23 +0300

    result page addded

--- redux/odev3/typingspeed/src/components/Results.js
+++ redux/odev3/typingspeed/src/components/Results.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Results() {
   return (
-    <div>Results</div>
+    <div className='sonuc'>
+        <div className='resultHeader'></div>
+        <h2>Results</h2>
+        <h1>0 WPM</h1>
+        <h6>(words per minute)</h6>
+        <br></br>
+        <hr></hr>
+        <h3>Keystrokes</h3>
+        <hr></hr>
+        <h3>Accuracy</h3>
+        <hr></hr>
+        <h3>Correct words</h3>
+        <hr></hr>
+        <h3>Wrong words</h3>
+        <hr></hr>
+
+    </div>
   )
 }
 
