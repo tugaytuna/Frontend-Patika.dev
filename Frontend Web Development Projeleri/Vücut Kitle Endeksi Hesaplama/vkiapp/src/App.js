aabbe8c42ec0dbaa5ed145196242b7c6d08f2f89
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-02-08 15:52:37 +0300

    calculator basics done

--- Frontend Web Development Projeleri/Vücut Kitle Endeksi Hesaplama/vkiapp/src/App.js
+++ Frontend Web Development Projeleri/Vücut Kitle Endeksi Hesaplama/vkiapp/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import BMIHesapla from './Components/BMIHesapla';
 import BMINedir from './Components/BMINedir';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
      <BMIHesapla>
      </BMIHesapla>
      <BMINedir></BMINedir>
--- Frontend Web Development Projeleri/Vücut Kitle Endeksi Hesaplama/vkiapp/src/Components/BMIHesapla.js
+++ Frontend Web Development Projeleri/Vücut Kitle Endeksi Hesaplama/vkiapp/src/Components/BMIHesapla.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function BMIHesapla() {
   return (
-    <div>BMIHesapla</div>
+    <div>
+      <label>Age</label>
+      <input type={"text"}></input>
+      <br></br>
+      <label>Gender: </label>
+      <input type={"radio"} id="female" name="Gender" value="Female" ></input>
+      <label for="female" >Female</label>
+      <input type={"radio"} id="male" name="Gender"  value="Male" ></input>
+      <label for="male" >Male</label>
+      <br></br>
+      <label>Height</label>
+      <input type={"text"}></input>
+      
+      <label>Weight</label>
+      <input type={"text"}></input>
+      
+
+      <hr></hr>
+
+    </div>
   )
 }
 
